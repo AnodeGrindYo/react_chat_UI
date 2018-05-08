b4d16df9df4bac303ef5b18850fe42a56f578a6b
adriengodoy <mr.a.godoy@gmail.com>
2018-05-08 10:40:04 +0200

    ajout de goToChat dans la classe Connexion dans src/components/Connexion.js

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 class Connexion extends React.Component {
 
+    goToChat = event => {
+        event.preventDefault()
+        // récupération du pseudo
+        console.log(this.pseudoInput.value)
+        // TODO : changement d'url
+    }
+
     render() {
         return (
             <div className="connexionBox">
-                <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required />
+                <form className="connexion" onSubmit={(e) => this.goToChat(e)} >
+                    <input type="text" placeholder="Pseudo" required ref={(input) => {this.pseudoInput = input}} />
                     <button type="submit">GO</button>
                 </form>
             </div>
