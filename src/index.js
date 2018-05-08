f1bed8a82d263f31fd324c23a596314832508dec
adriengodoy <mr.a.godoy@gmail.com>
2018-05-08 09:48:41 +0200

    la classe Connexion a été déplacée dans src/components/Connexion.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Connexion from './components/Connexion'
 // import CSS
 import './index.css'
-
-class Connexion extends React.Component {
-
-    render() {
-        return (
-            <div className="connexionBox">
-                <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required />
-                    <button type="submit">GO</button>
-                </form>
-            </div>
-        )
-    }
-}
 
 render(
     <Connexion />,
