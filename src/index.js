90da8325d2041ba2892e033e30966cea86d95172
adriengodoy <mr.a.godoy@gmail.com>
2018-05-08 10:31:52 +0200

    ajout de src/components/NotFound.js et des dépendances du projet (petit oubi de ma part -_-)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
+// les components
 import Connexion from './components/Connexion'
+import App from './components/App'
+import NotFound from './components/NotFound'
+// rooter
+import { BrowserRouter, Match, Miss } from 'react-router'
 // import CSS
 import './index.css'
 
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div> {/* wrapper dans une div pour que ça fonctionne */}
+                <Match exactly pattern="/" component={Connexion} />
+                <Match pattern="/pseudo/:pseudo" component={App} />
+                <Miss compoentn={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
 render(
-    <Connexion />,
+    <App />,
     document.getElementById('root')
 );
