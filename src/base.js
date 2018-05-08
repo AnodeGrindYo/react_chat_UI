dec2a4193fe8c17f27324248664d2a88912b9fb9
adriengodoy <mr.a.godoy@gmail.com>
2018-05-08 11:40:17 +0200

    reup base.js

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+    apiKey: "insert api key here",
+    authDomain: "yourAuthDomain.firebaseapp.com",
+    databaseURL: "https://YourDataBaseURL.firebaseio.com"
+})
+
+export default base
