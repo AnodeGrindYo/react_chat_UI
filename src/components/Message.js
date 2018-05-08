4e1f9b2e1db11f4bb2c84b4862c5001863d80ca6
adriengodoy <mr.a.godoy@gmail.com>
2018-05-08 10:01:30 +0200

    ajout de la classe App dans src/components/App.js qui contient l'interface du chat: la zone d'affichage des messages et le formulaire d'envoi des messages. Ajout de la classe Message dans src/components/Message.js et de la classe formulaire dans src/components/Formulaire.js

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Message extends React.component {
+    render() {
+        return (
+            <p className="user-message">
+                Pseudo: mon message de test
+            </p>
+        )
+    }
+}
+
+export default Message
